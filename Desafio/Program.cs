773a1ebed8198ef956ccdf767b25c10c05ae167a
Sami Daniel <130937402+sami-daniel@users.noreply.github.com>
2023-06-07 22:08:29 -0300

    Add files via upload
    Diretório completo, com os arquivos para o funcionamento da solução.

---
+++ Desafio/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Senha novaSenha = new Senha();
+            string senha = string.Empty;
+
+            Console.WriteLine("Olá! Bem-vindo ao verificador de senhas seguras!");
+            Console.WriteLine("(Caso queira parar de digitar, digite sair)");
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.Write("Insira suas senhas: (Digite uma em cada linha)");
+                while (true)
+                {
+                    try
+                    {
+                        senha = Console.ReadLine();
+                        Console.WriteLine();
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine();
+                        Console.Write("Insira sua senha novamente: ");
+                    }
+                    while (senha == string.Empty)
+                    {
+                        Console.WriteLine();
+                        Console.Write("Sua senha não pode ser vazia. Digite algo para a senha: ");
+                        senha = Console.ReadLine();
+                        Console.WriteLine();
+                    }
+                }
+                if (senha == "sair")
+                    break;
+                else
+                {
+                    novaSenha.ConjuntoSenha.Add(senha);
+                    novaSenha.VerificadorDeSenhas(senha);
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.Write($"Senhas digitadas: ");
+            foreach(string senhaDigitada in novaSenha.ConjuntoSenha)
+            {
+                Console.Write($"{senhaDigitada} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Senhas digitadas e consideradas seguras: ");
+            foreach (string senhaDigitada in novaSenha.ConjuntoSenhasSeguras)
+            {
+                Console.Write($"{senhaDigitada} ");
+            }
+            Console.WriteLine();
+           
+        }
+    }
+}
---
+++ Desafio/Senha.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio
+{
+    internal class Senha
+    {
+        public List<string> ConjuntoSenha = new List<string>();
+        public List<string> ConjuntoSenhasSeguras = new List<string>();
+
+        private List<char> ListaDeSimbolos = new List<char>()
+        {
+            '!', '@', '#', '$', '%', '&', '*', '+', '-', '=', '/', ',', '<', '>', '?', '|', '~'
+        };
+
+        public void VerificadorDeSenhas(string senha)
+        {
+            bool PossuiSeteCaracteres = false;
+            bool PossuiSimbolo = false;
+            bool PossuiLetraMaiuscula = false;
+            bool PossuiLetraMinuscula = false;
+
+            if (senha.Length >= 7)
+            {
+                PossuiSeteCaracteres = true;
+            }
+            foreach (char caractere in senha)
+            {
+                if (char.IsUpper(caractere))
+                    PossuiLetraMaiuscula = true;
+                else if (char.IsLower(caractere))
+                    PossuiLetraMinuscula = true;
+                else if (ListaDeSimbolos.Contains(caractere))
+                    PossuiSimbolo = true;
+            }
+            if (PossuiSeteCaracteres && PossuiSimbolo && PossuiLetraMaiuscula && PossuiLetraMinuscula)
+            {
+                ConjuntoSenhasSeguras.Add(senha);
+            }
+        }
+    }
+}
